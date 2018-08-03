25f832e8b00f78f4ac8863810660263aab53c05c
AberDerBart <nonatz@web.de>
2018-08-03 18:53:59 +0200

    further refactoring

--- recipemd-extract.py
+++ recipemd-extract.py
@@ -7,6 +7,7 @@ import codecs
 import sys
 import argparse
 from argparse import RawTextHelpFormatter
+from recipe import Recipe,Ingredient
 
 
 def chefkoch(soup):
@@ -32,35 +33,20 @@ def chefkoch(soup):
 	for row in rows:
 		cols = row.find_all('td')
 		cols = [s.text.strip() for s in cols]
-		if(cols[0] != ''):
-			ingreds.append('-*{}* {}'.format(cols[0],cols[1]))
-		else:
-			ingreds.append('-{}'.format(cols[1]))
+		ingreds.append(Ingredient(cols[1],cols[0]))
 	# instructions
 	instruct = soup.find('div', attrs={'id': 'rezept-zubereitung'}).text  # only get text
 	instruct = instruct.strip()  # remove leadin and ending whitespace
 	# write to file
-	writeFile(title, ingreds, instruct, summary, tags)
-
-def writeFile(title, ingreds, instruct, summary='', tags=[]):
-	with codecs.open(title.lower().replace(' ', '-') + '.md', 'w', encoding="utf-8") as f:
-		f.write('# ' + title + '\n\n')
-		if(summary != ''):
-			f.write('{}\n\n'.format(summary))
-		if(tags != []):
-			f.write('*{}*\n\n'.format(', '.join(tags)))
-		f.write('---\n\n')
-		f.write('\n'.join(ingreds))
-		f.write('\n\n---\n\n')
-		f.write(instruct)
-		print('File written as: "' + title.lower().replace(' ', '-') + '.md"')
-
+	return Recipe(title, ingreds, instruct, summary, tags)
 
 def main():
 	parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
 	parser.add_argument('url', help='URL of the recipe')
+	parser.add_argument('filename', help='the file to write to')
 	args = parser.parse_args()
 	url = args.url
+	filename = args.filename
 	try:
 		page = requests.get(url)
 	except Exception:
@@ -69,7 +55,7 @@ def main():
 	soup = BeautifulSoup(page.text, "html5lib")
 
 	if 'www.chefkoch.de/' in url:
-		chefkoch(soup)
+		chefkoch(soup).write(filename)
 	else:
 		print ('Website not supported')
 
