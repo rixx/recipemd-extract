abbefa840660d198271045f1b5d057ff5761f5fe
AberDerBart <nonatz@web.de>
2018-08-03 16:11:50 +0200

    refactoring

---
+++ recipe.py
@@ -0,0 +1,34 @@
+import codecs
+
+class Recipe:
+	def __init__(self, title, ingredients, instructions, summary='', tags=[]):
+		self.title=title
+		self.ingr=ingredients
+		self.instr=instructions
+		self.summary=summary
+		self.tags=tags
+	def write(self, filename=None):
+		if not filename:
+			filename = '{}.md'.format(self.title.lower().replace(' ', '-'))
+		with codecs.open(filename, 'w', encoding="utf-8") as f:
+			f.write('# ' + self.title + '\n\n')
+			if(self.summary != ''):
+				f.write('{}\n\n'.format(self.summary))
+			if(self.tags != []):
+				f.write('*{}*\n\n'.format(', '.join(self.tags)))
+			f.write('---\n\n')
+
+			for ingredient in self.ingr:
+				if ingredient.amount:
+					f.write('- *{}* {}\n'.format(ingredient.amount,ingredient.name))
+				else:
+					f.write('- {}\n'.format(ingredient.name))
+
+			f.write('\n---\n\n')
+			f.write(self.instr)
+			print('File written as: "{}"'.format(filename))
+
+class Ingredient:
+	def __init__(self, name, amount=None):
+		self.name=name
+		self.amount=amount
