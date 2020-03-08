442768953c19264e3d152cfae14e3ba10bdfc080
Tilman Stehr <tilman@tilman.ninja>
2020-03-08 18:18:18 +0100

    Create setup.py
    Fixes #9
    
    Note that some locations in setup.py (marked with TODO) still need to be filled out. These are:
    
    - version
    - classifiers

---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="recipemd-extract",
+	# TODO
+	version="TBD",
+	author='AberDerBart',
+	author_email='nonatz@web.de',
+	description="Extracts recipes from websites and saves them in the RecipeMD format",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	python_requires='>=3.7,<4',
+	install_requires=[
+		'recipemd~=4.0.3',
+		'beautifulsoup4~=4.7.1',
+		'requests~=2.22.0',
+		'html5lib==1.0.1',
+		'scrape-schema-recipe==0.0.4',
+		'recipe-scrapers==5.3.0',
+	],
+	entry_points={
+		'console_scripts': [
+			'recipemd-extract=recipemd_extract.main:main',
+		],
+	},
+	classifiers=[
+		# TODO
+		"Development Status :: 5 - Production/Stable",
+		"Environment :: Console",
+		"Intended Audience :: End Users/Desktop",
+		"Programming Language :: Python :: 3",
+		"Topic :: Utilities",
+		"Topic :: Text Processing :: Markup",
+		"Operating System :: OS Independent",
+	],
+	project_urls={
+		'Source': 'https://github.com/AberDerBart/recipemd-extract',
+	},
+)
