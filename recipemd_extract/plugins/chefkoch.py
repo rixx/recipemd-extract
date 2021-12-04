43d900ff2c95dd3fcc6586b4d299667a12409c77
Tobias Kunze <r@rixx.de>
2021-12-04 15:53:00 +0100

    Fix chefkoch plugin

--- recipemd_extract/plugins/chefkoch.py
+++ recipemd_extract/plugins/chefkoch.py
@@ -8,23 +8,23 @@ def extract(url,soup):
 		return
 
 	# title
-	title = soup.find('h1', attrs={'class': 'page-title'}).text
+	title = soup.find('h1').text
 	if title == 'Fehler: Seite nicht gefunden' or title == 'Fehler: Rezept nicht gefunden':
 		raise ValueError('No recipe found, check URL')
 	# summary
 	summaryTag = soup.find('div', attrs={'class': 'summary'})
 	summary = summaryTag.text if summaryTag else None
 	# servings
-	servings= soup.find('input', attrs={'id':'divisor'}).attrs['value']
+	servings= soup.find('input', attrs={'name':'portionen'}).attrs['value']
 	yields=[Amount(Decimal(servings), f'Portion{"en" if int(servings) > 1 else ""}')]
 
 	# tags
 	tags=[]
-	tagcloud=soup.find('ul', attrs={'class':'tagcloud'})
+	tagcloud=soup.find('amp-carousel', attrs={'class': 'ds-tags-carousel'})
 	for tag in tagcloud.find_all('a'):
-		tags.append(tag.text)
+		tags.append(tag.text.strip(" \n"))
 	# ingredients
-	table = soup.find('table', attrs={'class': 'incredients'})
+	table = soup.find('table', attrs={'class': 'ingredients'})
 	rows = table.find_all('tr')
 
 	ingreds=[]
@@ -34,7 +34,7 @@ def extract(url,soup):
 		amount = RecipeParser.parse_amount(cols[0])
 		ingreds.append(Ingredient(name=cols[1],amount=amount))
 	# instructions
-	instruct = soup.find('div', attrs={'id': 'rezept-zubereitung'}).text  # only get text
+	instruct = soup.select('small.ds-recipe-meta + div')[0].text  # only get text
 	instruct = instruct.strip()  # remove leadin and ending whitespace
 	# write to file
 	return Recipe(title=title, ingredients=ingreds, instructions=instruct, description=summary, tags=tags, yields=yields)
